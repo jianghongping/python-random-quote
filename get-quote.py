1bb01f095ce9836b19184836e610eb9e3fdfa143
Hoping <40732427+jianghongping@users.noreply.github.com>
2020-02-22 12:56:53 +0800

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,12 @@
+import random
 def primary():
 	print("Keep it logically awesome.")
 	f = open("quotes.txt")
 	quotes = f.readlines()
 	f.close()
-
-	print(quotes[0])
+	last = len(quotes)-1
+	rnd = rand.randint(0,last)
+	print(quotes[13])
 
 if __name__== "__main__":
 	primary()
